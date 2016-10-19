a614f869e33526c951834a139f70c6d66af0451b
Dmitry Guryanov <dguryanov@mirantis.com>
2016-10-19 15:40:39 +0300

    add file headers with license and fix pep8 errors

--- xyilib/tests/fake_camera.py
+++ xyilib/tests/fake_camera.py
@@ -1,8 +1,23 @@
+# Copyright 2016 Dmitry Guryanov <dmitry.guryanov@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import errno
 import json
 import socket
 import threading
+
+from six.moves import input
 
 from xyilib import consts
 from xyilib import errors
@@ -82,4 +97,4 @@ class FakeCamera(object):
 if __name__ == '__main__':
     camera = FakeCamera()
     camera.start()
-    raw_input()
+    input()
